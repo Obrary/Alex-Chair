5c401b6033f46f45fc482685f73ac7c059c122e8
Scott Austin <svaustin@gmail.com>
2015-07-13 16:33:55 -0700

    updated

---
+++ push.bat
@@ -0,0 +1,5 @@
+git remote add Portfolio-Cover https://github.com/Obrary/Portfolio-Cover
+git push --force Portfolio-Cover gh-pages
+
+git remote add Photo-Binder https://github.com/Obrary/Photo-Binder
+git push --force Photo-Binder gh-pages
